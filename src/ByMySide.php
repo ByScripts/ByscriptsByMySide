813cfa415f216edaf9c2162ce1a33d90081948f2
Thierry Goettelmann <thierry@byscripts.info>
2014-07-15 23:31:09 +0200

    Add phpdoc
    - Add some PhpDoc
    - Add hasLeftContainer/hasRightContainer methods

--- src/ByMySide.php
+++ src/ByMySide.php
@@ -9,42 +9,18 @@ class ByMySide
     const ICON_FORMAT_FONTAWESOME = '<span class="fa fa-%s"></span>';
 
     /**
+     * Icon format (printf compatible format)
+     *
      * @var string
      */
     private static $iconFormat = self::ICON_FORMAT_RAW;
 
     /**
+     * Predefined styles
+     *
      * @var array
      */
     private static $styles = [];
-
-    public static function setIconFormat($format)
-    {
-        self::$iconFormat = $format;
-    }
-
-    public static function buildIcon($icon, $format = null)
-    {
-        return sprintf($format ?: self::$iconFormat, $icon);
-    }
-
-    public static function addStyle($name, $scheme = null, $icon = null, $highlight = false, array $attributes = [])
-    {
-        self::$styles[ $name ] = compact('scheme', 'icon', 'highlight', 'attributes');
-    }
-
-    public static function applyStyle($name, ByMySideItem $item)
-    {
-        if (array_key_exists($name, self::$styles)) {
-
-            extract(self::$styles[ $name ]);
-
-            !empty($scheme) && $item->scheme($scheme);
-            !empty($icon) && $item->icon($icon);
-            !empty($highlight) && $item->highlight();
-            !empty($attributes) && $item->setAttributes($attributes);
-        }
-    }
 
     /**
      * @var ByMySideContainer
@@ -57,11 +33,85 @@ class ByMySide
     private $rightContainer;
 
     /**
+     * Set the icon format (printf compatible format)
+     *
+     * @param $format
+     */
+    public static function setIconFormat($format)
+    {
+        self::$iconFormat = $format;
+    }
+
+    /**
+     * Generate an icon
+     *
+     * @param string      $icon
+     * @param null|string $format
+     *
+     * @return string
+     */
+    public static function buildIcon($icon, $format = null)
+    {
+        return sprintf($format ?: self::$iconFormat, $icon);
+    }
+
+    /**
+     * Add a predefined style
+     *
+     * @param string $name       The name of the style
+     * @param null   $scheme     The color scheme to use
+     * @param null   $icon       The icon to use
+     * @param bool   $highlight  Highlight the icon?
+     * @param array  $attributes HTML attributes
+     */
+    public static function addStyle($name, $scheme = null, $icon = null, $highlight = false, array $attributes = [])
+    {
+        self::$styles[ $name ] = compact('scheme', 'icon', 'highlight', 'attributes');
+    }
+
+    /**
+     * Apply a predefined style to an item
+     *
+     * @param string       $name The name of the style to apply
+     * @param ByMySideItem $item The item to style
+     */
+    public static function applyStyle($name, ByMySideItem $item)
+    {
+        if (array_key_exists($name, self::$styles)) {
+
+            extract(self::$styles[ $name ]);
+
+            !empty($scheme) && $item->scheme($scheme);
+            !empty($icon) && $item->icon($icon);
+            !empty($highlight) && $item->highlight();
+            !empty($attributes) && $item->attributes($attributes);
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasLeftContainer()
+    {
+        return null !== $this->leftContainer;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasRightContainer()
+    {
+        return null !== $this->rightContainer;
+    }
+
+    /**
+     * Get the left container (created on first call)
+     *
      * @return ByMySideContainer
      */
     public function left()
     {
-        if (null === $this->leftContainer) {
+        if (!$this->hasLeftContainer()) {
             $this->leftContainer = new ByMySideContainer(ByMySideContainer::LEFT);
         }
 
@@ -69,11 +119,13 @@ class ByMySide
     }
 
     /**
+     * Get the right container (created on first call)
+     *
      * @return ByMySideContainer
      */
     public function right()
     {
-        if (null === $this->rightContainer) {
+        if (!$this->hasRightContainer()) {
             $this->rightContainer = new ByMySideContainer(ByMySideContainer::RIGHT);
         }
 
@@ -81,6 +133,8 @@ class ByMySide
     }
 
     /**
+     * Shortcut to get the top left block
+     *
      * @return ByMySideBlock
      */
     public function topLeft()
@@ -89,6 +143,8 @@ class ByMySide
     }
 
     /**
+     * Shortcut to get the bottom left block
+     *
      * @return ByMySideBlock
      */
     public function bottomLeft()
@@ -97,6 +153,8 @@ class ByMySide
     }
 
     /**
+     * Shortcut to get the top right block
+     *
      * @return ByMySideBlock
      */
     public function topRight()
@@ -105,6 +163,8 @@ class ByMySide
     }
 
     /**
+     * Shortcut to get the bottom right block
+     *
      * @return ByMySideBlock
      */
     public function bottomRight()
@@ -125,17 +185,19 @@ class ByMySide
     }
 
     /**
+     * Render the containers
+     *
      * @return string
      */
     public function render()
     {
         $output = '';
 
-        if(null !== $this->leftContainer) {
+        if ($this->hasLeftContainer()) {
             $output .= $this->leftContainer->render();
         }
 
-        if(null !== $this->rightContainer) {
+        if ($this->hasRightContainer()) {
             $output .= $this->rightContainer->render();
         }
 
